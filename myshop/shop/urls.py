aefdd939e273e3f073b5b10d3acf89a27db4fb24
irekpi <pilat.irek@gmail.com>
2020-02-04 13:31:12 +0100

    First commit of Online shop, MainSite with Cart and CartMenager is working

---
+++ myshop/shop/urls.py
@@ -0,0 +1,11 @@
+from django.conf.urls import url
+from django.urls import path
+from . import views
+
+
+app_name = 'shop'
+urlpatterns = [
+    path('', views.product_list, name='product_list'),
+    path('<category_slug>/', views.product_list, name='product_list_by_category'),
+    path('<id>/<slug>/', views.product_detail, name='product_detail'),
+]
