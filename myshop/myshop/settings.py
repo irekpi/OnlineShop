a2707410fad62a4cf1fdfbf5d713d2829279b53a
irekpi <pilat.irek@gmail.com>
2020-02-05 14:39:28 +0100

    Adding Order and Checkout functions, Celery is not yet working properly due to RabbitMQ issue

--- myshop/myshop/settings.py
+++ myshop/myshop/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     # Apps
     'shop',
     'cart',
+    'orders',
 ]
 
 MIDDLEWARE = [
@@ -128,3 +129,5 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 
 CART_SESSION_ID = 'cart'
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
